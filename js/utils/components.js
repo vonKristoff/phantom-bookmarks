37ffe8fb0eef9f36fbc0b8946ae291ef1214bf1c
JC Nicolas <mrjcnicolas@gmail.com>
2019-02-14 15:41:55 +0000

    add helper utils

---
+++ js/utils/components.js
@@ -0,0 +1,17 @@
+import overview from '../components/overview'
+import editor from '../components/editor'
+import response from '../components/response'
+import footer from '../components/footer'
+const methods = {
+	overview,
+	editor,
+	response,
+	footer
+}
+export default {
+	make(obj) {
+		let name = Object.keys(obj).pop()
+		let el = obj[name]
+		return Object.assign(methods[name], { name, parent: this, el, children: {}, markup: {}, mounted: false })
+	}
+}
---
+++ js/utils/database.js
@@ -0,0 +1,19 @@
+// first run bookmarks database
+export default [
+	{
+		id: 0,
+		label: "Blog of Cats",
+		url: "http://random.cat",
+		tags: []
+	},{
+		id: 1,
+		label: "Blog of Dogs",
+		url: "",
+		tags: []
+	},{
+		id: 2,
+		label: "Blog of Witches",
+		url: "",
+		tags: []
+	}
+]
