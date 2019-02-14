a5eda30f0f9e1741e487ef3db1173a6f0f76b5b9
JC Nicolas <mrjcnicolas@gmail.com>
2019-02-14 16:27:22 +0000

    update build to utilise uglify and compress that bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import Commonjs from 'rollup-plugin-commonjs'
 import Resolve from 'rollup-plugin-node-resolve'
 import Buble from 'rollup-plugin-buble'
-import Uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 import { eslint } from 'rollup-plugin-eslint'
 import replace from 'rollup-plugin-replace'
 import html from 'rollup-plugin-html'
@@ -33,7 +33,7 @@ export default {
         }),
         Commonjs({ include: 'node_modules/**' }),
         replace({ exclude: 'node_modules/**', ENV: JSON.stringify(process.env.NODE_ENV || 'development') }),        
-        (process.env.NODE_ENV === 'production' && Uglify({}, minify))
+        uglify()
     ],
     output: { format, name, file, sourcemap }
 }
